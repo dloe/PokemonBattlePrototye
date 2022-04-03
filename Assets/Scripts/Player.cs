23b7433a88fbefb598a7d3575b01b3bb04f3f0c2
Dylan Loe <dylanloe@gmail.com>
2022-04-03 08:47:33 -0700

    Basic Main Menu
    (for playability)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// Player.cs
+    /// Dylan Loe
+    /// 
+    /// Last Updated: 4/3/22
+    /// 
+    /// 
+    /// Notes:
+    /// - Contains Player Controls
+    /// - UI Hookup (most likely will seperate this)
+    /// - CHECK LATER" Maybe contrain player data
+    ///     - player data includes current monster party
+    ///     
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
