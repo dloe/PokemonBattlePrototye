e3717546713d1e0ff85585cbf6b5c6e57821a499
Olah98 <aolah@mail.csuchico.edu>
2022-04-03 16:13:25 -0700

    Created Battle Manager
    Created the script that will manage the battle this will be thing such as initialize the battle and manage the turn order.

---
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    /// <summary>
+    /// BattleManager.cs
+    /// 
+    /// Updated: 4/3/22
+    /// 
+    /// Notes:
+    /// - Manages all aspects of a battle
+    /// 
+    /// - 
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// This will set the stage for the battle
+    /// </summary>
+    void initializeBattle()
+    {
+
+    }
+}
