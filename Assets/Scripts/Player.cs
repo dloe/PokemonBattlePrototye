218a48fed6d25c2a93ddf1337b63f501573de64c
Dylan Loe <dylanloe@gmail.com>
2022-04-03 14:41:28 -0700

    Finished Player Inventory/party config
    - happens in PlayerInventory.json

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEditor;
 
 public class Player : MonoBehaviour
 {
@@ -24,30 +25,19 @@ public class Player : MonoBehaviour
     /// </summary>
 
     public PlayerParty myPlayerParty;
-    public MovesStorage test;
+    public MovesStorage moveList;
     string path;
     
 
     // Start is called before the first frame update
     void Awake()
     {
-        //loading json test
-        path = Application.dataPath + "/MoveData.JSON";
-        if(File.Exists(path))
-        {
-            string content = File.ReadAllText(path);
+        
+        
 
-            test = new MovesStorage();
-            //SaveData();
-            LoadData(content);
-        }
-        else
-        {
-            Debug.Log("Check path");
-        }
-
-
-        LoadParty();
+        SetUpMoveData();
+        SetUpPlayerInfo();
+       
     }
 
     // Update is called once per frame
@@ -56,16 +46,42 @@ public class Player : MonoBehaviour
         
     }
 
+
+    #region SetupInfo
+
+    void SetUpMoveData()
+    {
+        moveList = new MovesStorage();
+        //loading json test
+        path = Application.dataPath + "/MoveData.JSON";
+        if (File.Exists(path))
+        {
+
+            string content = File.ReadAllText(path);
+            //SaveData();
+            LoadData(content);
+        }
+        else
+        {
+            Debug.Log("Check path");
+        }
+    }
+
+    void SetUpPlayerInfo()
+    {
+        myPlayerParty = new PlayerParty();
+        //SaveParty();
+        LoadParty();
+    }
+
     //get move lists - will rework but this works and we will use it for setting up the pokemons move sets
     public void LoadData(string moveData)
     {
-
+        
         //JsonUtility.FromJsonOverwrite(moveData, this);
-        test = JsonUtility.FromJson<MovesStorage>(moveData);
-        Debug.Log("Move Data Loaded: ");
-        Debug.Log(test.moveList[0].moveName);
+        moveList = JsonUtility.FromJson<MovesStorage>(moveData);
+        Debug.Log("Move Data Loaded...");
     }
-
 
     public void LoadParty()
     {
@@ -73,13 +89,13 @@ public class Player : MonoBehaviour
         if (File.Exists(path))
         {
             string content = File.ReadAllText(path);
-            myPlayerParty = new PlayerParty();
+            
             //JsonUtility.FromJsonOverwrite(content, this);
             myPlayerParty = JsonUtility.FromJson<PlayerParty>(content);
-            Debug.Log("Party Data Loaded:");
-            Debug.Log(myPlayerParty.wins);
-            Debug.Log(myPlayerParty.party[0].dexNum);
-            Debug.Log(myPlayerParty.party[0].mName);
+            Debug.Log("Party Data Loaded...");
+            //Debug.Log(myPlayerParty.wins);
+            //Debug.Log(myPlayerParty.party[0].dexNum);
+            //Debug.Log(myPlayerParty.party[0].mName);
         }
         else
         {
@@ -88,8 +104,30 @@ public class Player : MonoBehaviour
         }
     }
 
+    public void SaveParty()
+    {
+        path = Application.dataPath + "/PlayerInventory.JSON";
+        
 
+        Monster m = new Monster();
+        m.currentHealth = 100;
+        m.mName = "MONSTER TEST";
+        myPlayerParty.party[0] = m;
 
+        string json = JsonUtility.ToJson(myPlayerParty);
+        Debug.Log(json);
+
+        if (File.Exists(path))
+        {
+            //string content = File.ReadAllText(path.ToString());
+            //Debug.Log(content);
+            File.WriteAllText(path, json);
+        }
+        else
+        {
+            Debug.Log("problem");
+        }
+    }
 
     //temp testing of json format, will reference later for saving out playe stats and whatnot
     public void SaveData()
@@ -106,13 +144,15 @@ public class Player : MonoBehaviour
         m.moveDescription = "this is test prey it works";
 
         //moveListT[0] = m;
-        test.moveList[0] = m;
+        moveList.moveList[0] = m;
 
-        string json = JsonUtility.ToJson(test);
+        string json = JsonUtility.ToJson(moveList);
         Debug.Log(json);
 
         File.WriteAllText(path, json);
 
 
     }
+
+    #endregion
 }
