514779634f49510d492352029dd0807c6253883a
Dylan Loe <dylanloe@gmail.com>
2022-04-03 16:57:13 -0700

    Start of ai

---
+++ Assets/Scripts/AI/AI_Lvl1_WildEncounter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Lvl1_WildEncounter : MonoBehaviour
+{
+    /// <summary>
+    /// AI_Lvl1_WildEncounter
+    /// Dylan Loe
+    /// 
+    /// Updated: 4/3/22
+    /// 
+    /// - Level 1 AI - Used Specifically for Wild Pokemon Encounters
+    /// 
+    /// - has the same percent chance to pick from any of its active moves
+    ///     - so for example if we have 4 moves for a bidoof, it has a 25% to pick any of them
+    /// - will have a function to set up start of encounter for what pokemon (whwat moves, stats etc)
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
